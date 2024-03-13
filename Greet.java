3647d0fd0cfd10563d1c0ea9a7e8a1dd280004ad
Dmitrij Zlobicki <dmitrij.zlobicki@gmail.com>
2024-03-13 09:08:05 +0000

    Greet main commit

--- Greet.java
+++ Greet.java
@@ -1,9 +1,5 @@
 public class Greet {
 public static void main(String[] args) {
-<<<<<<< HEAD
 System.out.println("Hello World changed for main branch!");
-=======
-System.out.println("Hello World changed for conflict branch!");
->>>>>>> new-conflict-branch
 }
 }
